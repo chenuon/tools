8cf26796b400edbf853c496e9d731eea531e2127
chenuon <59986158+chenuon@users.noreply.github.com>
2025-03-24 13:31:09 +0800

    Update NC_QB504.sh

--- NC_QB438.sh
+++ NC_QB438.sh
@@ -7,31 +7,18 @@ fi
 
 USER=$1
 PASSWORD=$2
-PORT=${3:-8080}
+PORT=${3:-9527}
 UP_PORT=${4:-23333}
 RAM=$(free -m | awk '/^Mem:/{print $2}')
 CACHE_SIZE=$((RAM / 8))
 
-bash <(wget -qO- https://raw.githubusercontent.com/jerry048/Dedicated-Seedbox/main/Install.sh) -u $USER -p $PASSWORD -c $CACHE_SIZE -q 4.3.9 -l v1.2.19 -x
-apt install -y curl htop vnstat
+bash <(wget -qO- https://raw.githubusercontent.com/jerry048/Dedicated-Seedbox/main/Install.sh) -u $USER -p $PASSWORD -c $CACHE_SIZE -q 4.6.7 -l v1.2.20
 systemctl stop qbittorrent-nox@$USER
 systemctl disable qbittorrent-nox@$USER
-systemARCH=$(uname -m)
-if [[ $systemARCH == x86_64 ]]; then
-    wget -O /usr/bin/qbittorrent-nox https://raw.githubusercontent.com/guowanghushifu/Seedbox-Components/refs/heads/main/Torrent%20Clients/qBittorrent/x86_64/qBittorrent-4.3.8%20-%20libtorrent-v1.2.14/qbittorrent-nox
-elif [[ $systemARCH == aarch64 ]]; then
-    wget -O /usr/bin/qbittorrent-nox https://raw.githubusercontent.com/guowanghushifu/Seedbox-Components/refs/heads/main/Torrent%20Clients/qBittorrent/ARM64/qBittorrent-4.3.8%20-%20libtorrent-v1.2.14/qbittorrent-nox
-fi
+sudo cp /usr/bin/qbittorrent-nox /usr/bin/qbittorrent-nox1
+wget -O /usr/bin/qbittorrent-nox wget https://github.com/chenuon/jscode/releases/download/v5.0.4/qbittorrent-nox
 chmod +x /usr/bin/qbittorrent-nox
-sed -i "s/WebUI\\\\Port=[0-9]*/WebUI\\\\Port=$PORT/" /home/$USER/.config/qBittorrent/qBittorrent.conf
-sed -i "s/Connection\\\\PortRangeMin=[0-9]*/Connection\\\\PortRangeMin=$UP_PORT/" /home/$USER/.config/qBittorrent/qBittorrent.conf
-sed -i "/\\[Preferences\\]/a General\\\\Locale=zh" /home/$USER/.config/qBittorrent/qBittorrent.conf
-sed -i "/\\[Preferences\\]/a Downloads\\\\PreAllocation=false" /home/$USER/.config/qBittorrent/qBittorrent.conf
-sed -i "/\\[Preferences\\]/a WebUI\\\\CSRFProtection=false" /home/$USER/.config/qBittorrent/qBittorrent.conf
-sed -i "s/disable_tso_/# disable_tso_/" /root/.boot-script.sh
-echo "systemctl enable qbittorrent-nox@$USER" >> /root/BBRx.sh
-echo "systemctl start qbittorrent-nox@$USER" >> /root/BBRx.sh
-echo "shutdown -r +1" >> /root/BBRx.sh
-tune2fs -m 1 $(df -h / | awk 'NR==2 {print $1}') 
-echo "接下来将自动重启2次，流程预计5-10分钟..."
-shutdown -r +1
+sudo apt update -y
+sudo apt upgrade -y
+sleep 3
+reboot
