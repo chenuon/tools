ef137cadd12064b79e80dd7a65c665bb1f413345
chenuon <59986158+chenuon@users.noreply.github.com>
2025-03-24 14:21:45 +0800

    Update nc_bbr.sh

--- nc_bbr.sh
+++ nc_bbr.sh
@@ -2,4 +2,6 @@
 
 bash <(wget -qO- https://raw.githubusercontent.com/guowanghushifu/Seedbox-Components/refs/heads/main/BBR/BBRx/BBRy.sh)
 sleep 3
+bash <(wget -qO- "https://net1999.net/misc/vnstat.sh")
+sleep 3
 reboot
