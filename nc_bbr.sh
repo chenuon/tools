475862308453bb95489ab5937001468b762d3a28
chenuon <59986158+chenuon@users.noreply.github.com>
2025-03-24 14:22:33 +0800

    Update nc_bbr.sh

--- nc_bbr.sh
+++ nc_bbr.sh
@@ -4,4 +4,6 @@ bash <(wget -qO- https://raw.githubusercontent.com/guowanghushifu/Seedbox-Compon
 sleep 3
 bash <(wget -qO- "https://net1999.net/misc/vnstat.sh")
 sleep 3
+systemctl start vnstat
+sleep 3
 reboot
